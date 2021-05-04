f431ef80258822c80ed2203deed796d386355e04
joyun <w2039@e-mirim.hs.kr>
2021-05-04 20:00:11 +0900

    Finish Chapter4_9Ex

--- app/src/main/java/kr/hs/emirim/chapter4_9ex/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chapter4_9ex/MainActivity.java
@@ -3,12 +3,29 @@ package kr.hs.emirim.chapter4_9ex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgv;
+    int angle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnRoate = findViewById(R.id.btn_rotate);
+        imgv = findViewById(R.id.imgv);
+
+        btnRoate.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener(){
+        public void onClick(View v){
+            angle += 10;
+            imgv.setRotation(angle);
+        }
+    };
 }
