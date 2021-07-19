b5912b370f0082c8e34066d74f2c47639abff5ce
joyun <w2039@e-mirim.hs.kr>
2021-07-19 09:39:56 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/project10_1/SecondsAcivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.project10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondsAcivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seconds);
+    }
+}
