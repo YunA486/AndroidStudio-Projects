8f0d2ff1f54c60c57ce955abc5a5966781a69c56
joyun <w2039@e-mirim.hs.kr>
2021-07-21 15:23:28 +0900

    Completion Project_BMI Commit

--- app/src/main/java/kr/hs/emirim/project_bmi/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/project_bmi/ResultActivity.java
@@ -24,27 +24,35 @@ public class ResultActivity extends AppCompatActivity {
 
         String bmiStr = "";
         int imgSrcId = 0;
+        int bmiCommentId = 0;
 
         if(bmi < 18.5){
             bmiStr = "저";
+            bmiCommentId = R.string.bmi_comment1;
             imgSrcId = R.drawable.bmi_1;
         }else if(bmi < 24){
             bmiStr = "정상";
+            bmiCommentId = R.string.bmi_comment2;
             imgSrcId = R.drawable.bmi_2;
         }else if(bmi < 30){
             bmiStr = "과";
+            bmiCommentId = R.string.bmi_comment3;
             imgSrcId = R.drawable.bmi_3;
         }else if(bmi < 35){
             bmiStr = "비만";
+            bmiCommentId = R.string.bmi_comment4;
             imgSrcId = R.drawable.bmi_4;
         }else if(bmi < 40){
             bmiStr = "고도비만";
+            bmiCommentId = R.string.bmi_comment5;
             imgSrcId = R.drawable.bmi_5;
         }else{
             bmiStr = "초고도비만";
+            bmiCommentId = R.string.bmi_comment6;
             imgSrcId = R.drawable.bmi_6;
         }
         imgV.setImageResource(imgSrcId);
-        text1.setText(bmiStr + "체중");
+        text1.setText(String.format("귀하의 BMI지수는 %.2f(㎏/㎡)이므로 %s 체중입니다.", bmi, bmiStr));
+        text2.setText(bmiCommentId);
     }
 }
