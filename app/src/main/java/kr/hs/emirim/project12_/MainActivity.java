c654ec50c0ea549d7bd49ee6872e61d361a353e1
joyun <w2039@e-mirim.hs.kr>
2021-07-28 14:29:04 +0900

    edit MainActivity.java(직접 풀어보기12_2) Commit

--- app/src/main/java/kr/hs/emirim/project12_/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/project12_/MainActivity.java
@@ -29,6 +29,28 @@ public class MainActivity extends AppCompatActivity {
         Button btnInit = findViewById(R.id.btn_init);
         Button btnInput = findViewById(R.id.btn_input);
         Button btnSearch = findViewById(R.id.btn_search);
+        Button btnDelete = findViewById(R.id.btn_delete);
+        Button btnUpdate = findViewById(R.id.btn_update);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db = dbHelper.getWritableDatabase();
+                db.execSQL("update groupTB set count = "+editCount.getText().toString()+" where name='"+editName.getText().toString()+"';");
+                selectDB();
+                db.close();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db = dbHelper.getWritableDatabase();
+                db.execSQL("delete from groupTB where name = '"+editName.getText().toString()+"';");
+                selectDB();
+                db.close();
+            }
+        });
 
         dbHelper = new MyDBHelper(this);
         btnInit.setOnClickListener(new View.OnClickListener() {
@@ -36,9 +58,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 db = dbHelper.getWritableDatabase();
                 dbHelper.onUpgrade(db, 1, 2);
+                selectDB();
                 db.close();
             }
         });
+
         btnInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,6 +93,23 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void selectDB(){
+        db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from groupTB", null);
+        String strName = "그룹 이름\r\n________\r\n";
+        String strCount = "인원\r\n________\r\n";
+        while(cursor.moveToNext()){
+            strName += cursor.getString(0) + "\r\n";
+            strCount += cursor.getInt(1) + "\r\n";
+        }
+
+        editNameResult.setText(strName);
+        editCountResult.setText(strCount);
+
+        cursor.close();
+        db.close();
     }
 
     public class MyDBHelper extends SQLiteOpenHelper{
